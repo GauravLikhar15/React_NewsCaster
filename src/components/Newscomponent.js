b2d2b638ff4c77f559dce1807aff2c348c1e827e
Gaurav <Gaurav@gmail.com>
2022-03-13 00:36:39 +0530

    Tutorial 25

--- src/components/Newscomponent.js
+++ src/components/Newscomponent.js
@@ -2,6 +2,11 @@ import React ,{Component} from 'react'
 import Newsitem from './Newsitem'
 export default class newscomponent extends Component {
 
+        
+        
+        constructer(){
+                console.log("This is constructer");
+        }
   render() {
     return (
         <>
@@ -9,7 +14,7 @@ export default class newscomponent extends Component {
         <div className="container">
                 <div className="row">
                         <div className="col-md-4">
-                                <Newsitem title="First" discription="This is the first news"/>
+                                <Newsitem title="First" discription="This is the first news"imgURL="https://upload.wikimedia.org/wikipedia/commons/thumb/6/62/BBC_News_2019.svg/1200px-BBC_News_2019.svg.png" newsURL="To do"/>
                         </div>
                         <div className="col-md-4">
                                 <Newsitem title="Second" discription="This is the second news"/>
--- src/components/Newsitem.js
+++ src/components/Newsitem.js
@@ -2,15 +2,16 @@ import React,{Component} from 'react'
 
 export default class newsitem extends Component {
         render() {
-                let {title,discription}=this.props;
+                let {title,discription,imgURL,newUrl}=this.props;
     return (
         <div className="container my-3">
           
         <div className="card" style={{width: "18rem"}}>
-        <img src="" className="card-img-top" alt="..."/>
+        <img src={imgURL} className="card-img-top" alt="..."/>
         <div className="card-body">
           <h5 className="card-title">{title}</h5>
           <p className="card-text">{discription}</p>
+          <a href="" className='btn btn-sm btn-primary'>Read More</a>
         </div>
       </div>
       </div>
